d3a851249ec7d86368df14982c90f0dfbdc9780a
MK <masamichi.kagaya@gmail.com>
2019-03-31 10:53:39 +0900

    4th exce - finish 05

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,30 +1,15 @@
-// const panels = document.querySelectorAll('.panel');
-
-// function opneHandler() {
-//     let [ firstStat, secondStat, ] = ['open', 'open-active', ];
-//     // let stateflag = this.classList.contains('open');
-//     // if (stateflag) [ firstStat, secondStat, ] = [ 'open-active', 'open', ];
-
-//     this.classList.toggle(firstStat);
-//     setTimeout(() => {
-//         this.classList.toggle(secondStat);
-//     }, 700);
-// }
-// panels.forEach(panel => panel.addEventListener('click', opneHandler));
-
 const panels = document.querySelectorAll('.panel');
 
-function toggleOpen() {
-        this.classList.toggle('open');
+function panelToggle() {
+  this.classList.toggle('open');
 }
 
-function toggleActive(e) {
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-  }
+function panelSubtitleToggle(e) {
+  let flag = this.classList.contains('open');
+  if (e.propertyName.includes('flex') && flag) this.classList.add('open-active');
+  else this.classList.remove('open-active');
 }
 
-panels.forEach(panel => panel.addEventListener('mouseenter', toggleOpen));
-panels.forEach(panel => panel.addEventListener('mouseleave', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
-
+panels.forEach(panel => panel.addEventListener('mouseenter', panelToggle));
+panels.forEach(panel => panel.addEventListener('mouseleave', panelToggle));
+panels.forEach(panel => panel.addEventListener('transitionend', panelSubtitleToggle));
