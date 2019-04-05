9fe9babd5d3ae75d460d49de99e27b9244bcf2af
MK <masamichi.kagaya@gmail.com>
2019-04-06 07:15:10 +0900

    Three.js - Before making Demo

--- 31 - ThreeJs/script.js
+++ 31 - ThreeJs/script.js
@@ -7,61 +7,60 @@ let renderer;
 let mesh;
 let controls;
 
-// function init() {
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
-renderer = createRenderer(width, height);
-window.addEventListener('resize', () => {
-	let width = window.innerWidth;
-	let height = window.innerHeight;
-	renderer.setSize(width, height);
-	camera.aspect = width / height;
-	camera.updateProjectionMatrix();
-});
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
+	renderer = createRenderer(width, height);
+	window.addEventListener('resize', () => {
+		let width = window.innerWidth;
+		let height = window.innerHeight;
+		renderer.setSize(width, height);
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+	});
 
-mesh = createMesh(side);
-camera.position.set(1, 20, 20);
+	mesh = createMesh(side);
+	camera.position.set(1, 20, 20);
 
-let loader = new THREE.GLTFLoader();
-loader.load('glTF/slipper_test_glTF.gltf', (gltf) => {
-	scene.add(gltf.scene);
-}, undefined, (error) => {
-	console.error(error);
-});
+	let loader = new THREE.GLTFLoader();
+	loader.load('glTF/slipper_test_glTF.gltf', (gltf) => {
+		scene.add(gltf.scene);
+	}, undefined, (error) => {
+		console.error(error);
+	});
 
 
-let ambientLight = new THREE.AmbientLight(0xFFFFFF, 3);
-scene.add(ambientLight);
+	let ambientLight = new THREE.AmbientLight(0xFFFFFF, 3);
+	scene.add(ambientLight);
 
-controls = new THREE.OrbitControls(camera);
-controls.autoRotateSpeed = 0.4;
-controls.autoRotate = true;
-update();
-// }
+	controls = new THREE.OrbitControls(camera);
+	controls.autoRotateSpeed = 0.4;
+	controls.autoRotate = true;
+	update();
 
-function createRenderer(width, height) {
-	let renderer = new THREE.WebGLRenderer({ antialias: true, });
-	renderer.setSize(width, height);
-	renderer.doubleSided = true;
-	renderer.gammaOutput = true; // !important! Making contrast normal.
-	document.body.appendChild(renderer.domElement);
-	return renderer;
+
+	function createRenderer(width, height) {
+		let renderer = new THREE.WebGLRenderer({ antialias: true, });
+		renderer.setSize(width, height);
+		renderer.doubleSided = true;
+		renderer.gammaOutput = true; // !important! Making contrast normal.
+		document.body.appendChild(renderer.domElement);
+		return renderer;
+	}
+
+	function createMesh(radius) {
+		let geometry = new THREE.IcosahedronGeometry(radius);
+		let material = new THREE.MeshNormalMaterial();
+		let mesh = new THREE.Mesh(geometry, material);
+		return mesh;
+	}
+
+	function update() {
+		controls.update();
+		requestAnimationFrame(update);
+		renderer.render(scene, camera);
+	}
 }
 
-function createMesh(radius) {
-	let geometry = new THREE.IcosahedronGeometry(radius);
-	let material = new THREE.MeshNormalMaterial();
-	let mesh = new THREE.Mesh(geometry, material);
-	return mesh;
-}
-
-function update() {
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
-	controls.update();
-	requestAnimationFrame(update);
-	renderer.render(scene, camera);
-}
-
-// window.addEventListener('DOMContentLoaded', init);
+window.addEventListener('DOMContentLoaded', init);
 
---
+++ 32 - ThreeJs - Demo/script.js
@@ -0,0 +1,40 @@
+// forked from FumioNonaka's "three.js r85: Rotating a icosahedron" http://jsdo.it/FumioNonaka/gpdN
+var width = window.innerWidth;
+var height = window.innerHeight;
+var side = Math.min(width, height) / 30;
+var scene;
+var camera;
+var renderer;
+var mesh;
+var controls;
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
+	renderer = createRenderer(width, height);
+	mesh = createMesh(side);
+	camera.position.z = 100;
+	scene.add(mesh);
+	controls = new THREE.OrbitControls(camera);
+	controls.autoRotate = true;
+	update();
+}
+function createRenderer(width, height) {
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+	return renderer;
+}
+function createMesh(radius) {
+	var geometry = new THREE.IcosahedronGeometry(radius);
+	var material = new THREE.MeshNormalMaterial();
+	var mesh = new THREE.Mesh(geometry, material);
+	return mesh;
+}
+function update() {
+	// mesh.rotation.x += 0.01;
+	// mesh.rotation.y += 0.01;
+	controls.update();
+	requestAnimationFrame(update);
+	renderer.render(scene, camera);
+}
+window.addEventListener('DOMContentLoaded', init);
