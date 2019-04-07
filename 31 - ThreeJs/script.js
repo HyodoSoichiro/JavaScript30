cc3e8245d706000f82a73fa9c82e58bfb1a5586e
MK <masamichi.kagaya@gmail.com>
2019-04-07 13:06:00 +0900

    Three.js 31 - After filming in JP

--- 31 - ThreeJs/script.js
+++ 31 - ThreeJs/script.js
@@ -6,11 +6,27 @@ let camera;
 let renderer;
 let mesh;
 let controls;
-
 function init() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(30, width / height, 1, 1000);
 	renderer = createRenderer(width, height);
+	renderer.gammaOutput = true; // !important! Making contrast normal.
+	let loader = new THREE.GLTFLoader();
+	    loader.load('glTF/slipper.gltf', (gltf) => {
+	        scene.add(gltf.scene);
+	    }, undefined, (error) => {
+	        console.error(error);
+    });
+
+	let ambientLight = new THREE.AmbientLight(0xFFFFFF, 3);
+	scene.add(ambientLight);
+
+	camera.position.set(1, 20, 20);
+	controls = new THREE.OrbitControls(camera);
+	controls.autoRotate = true;
+	controls.autoRotateSpeed = 0.6;
+	update();
+
 	window.addEventListener('resize', () => {
 		let width = window.innerWidth;
 		let height = window.innerHeight;
@@ -18,42 +34,17 @@ function init() {
 		camera.aspect = width / height;
 		camera.updateProjectionMatrix();
 	});
-
-	camera.position.set(1, 20, 20);
-
-	let loader = new THREE.GLTFLoader();
-	loader.load('glTF/slipper_test_glTF.gltf', (gltf) => {
-		scene.add(gltf.scene);
-	}, undefined, (error) => {
-		console.error(error);
-	});
-
-
-	let ambientLight = new THREE.AmbientLight(0xFFFFFF, 3);
-	scene.add(ambientLight);
-
-	controls = new THREE.OrbitControls(camera);
-	controls.autoRotateSpeed = 0.4;
-	controls.autoRotate = true;
-	update();
-
-
-	function createRenderer(width, height) {
-		let renderer = new THREE.WebGLRenderer({ antialias: true, });
-		renderer.setSize(width, height);
-		renderer.doubleSided = true;
-		renderer.gammaOutput = true; // !important! Making contrast normal.
-		document.body.appendChild(renderer.domElement);
-		return renderer;
-	}
-
-
-	function update() {
-		controls.update();
-		requestAnimationFrame(update);
-		renderer.render(scene, camera);
-	}
+}
+function createRenderer(width, height) {
+	let renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+	return renderer;
 }
 
+function update() {
+	controls.update();
+	requestAnimationFrame(update);
+	renderer.render(scene, camera);
+}
 window.addEventListener('DOMContentLoaded', init);
-
